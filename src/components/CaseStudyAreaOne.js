9c477db055c714287fc17dbf229fc1740b3b96fb
dheeraj303 <singh.dheeraj6303@gmail.com>
2024-11-12 12:18:20 +0530

    home page update

--- src/components/CaseStudyAreaOne.js
+++ src/components/CaseStudyAreaOne.js
@@ -6,24 +6,35 @@ const CaseStudyAreaOne = () => {
   return (
     <>
       {/*================== case-study area start ==================*/}
-      <div className="case-study-area bg-relative pd-top-110">
-        <img
+      <div className="case-study-area bg-relative pd-top-110 mb-3">
+        {/* <img
           className="animate-img-1 top_image_bounce"
           src="assets/img/banner/2.png"
           alt="img"
-        />
+        /> */}
         <div className="container">
           <div className="section-title text-center">
-            <h6 className="sub-title">GALLERY HERE</h6>
+            {/* <h6 className="sub-title">GALLERY HERE</h6> */}
             <h2 className="title">
-            Why  <span>Nyas </span> Consultants
+            Why  <span>Choose </span> Us
             </h2>
           </div>
           <div className="row">
            <div className="col-lg-12 col-md-12 mx-4 text-center">
-               <p>
-               At our company, we provide innovative solutions that bring creative ideas to challenge conventional norms, ensuring your business stands out in a competitive landscape. Our results-driven approach focuses on delivering real, measurable outcomes that enhance your business performance and foster overall growth. You can benefit from affordable expertise through our high-quality consultancy services offered at competitive rates, making professional support accessible without compromising quality. Our streamlined process guarantees quick onboarding and tailored expert assistance, allowing you to concentrate on your core business while we manage the technical details. With diverse digital solutions ranging from software development to mobile and full stack development, our custom offerings are designed to drive digital transformation and meet your specific needs.
-               </p>
+               <p className='text-center'>
+               Tailored Solutions for Every Business We understand that every business is unique. Our approach is personalized,
+with solutions crafted to meet your specific operational needs and growth objectives. </p>
+
+          <br></br>
+          <p className='text-center'>Experienced Professionals Our team of operations consultants and virtual assistants brings extensive experience
+across various industries, ensuring top-quality service and effective problem-solving.
+</p>
+<br></br>
+          <p className='text-center'>Focus on Efficiency and Cost-Savings Our strategies focus on eliminating waste, improving productivity, and
+          reducing costs, helping your business achieve more with less.</p>
+<br></br>
+          <p className='text-center'>Data-Driven and Results-Oriented With a strong focus on performance metrics and data-driven decision-making,
+          we provide insights that align with your long-term goals, allowing for sustained improvements.</p>
            </div>
             
           </div>
